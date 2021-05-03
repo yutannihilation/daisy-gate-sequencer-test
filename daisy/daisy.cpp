94b53a8b963582fc73b4ff91a21b4d4ac4052be5
Hiroaki Yutani <yutani.ini@gmail.com>
2021-05-03 21:08:10 +0900

    Use .clang-format

--- daisy/daisy.cpp
+++ daisy/daisy.cpp
@@ -1,23 +1,25 @@
-#include "daisysp.h"
 #include "daisy_seed.h"
+#include "daisysp.h"
 
 #define PIN_GATE_OUT1 13
 #define PIN_GATE_OUT2 14
 
-#define PIN_CONTROLS_BASE 15
-#define OFFSET_BITCRUSH_CONTROL 0
-#define OFFSET_REVERB_CONTROL1 1
-#define OFFSET_REVERB_CONTROL2 2
-#define OFFSET_REVERB_CONTROL3 3
-#define OFFSET_REVERB_CONTROL4 4
+// clang-format off
+#define PIN_CONTROLS_BASE       15
+#define OFFSET_BITCRUSH_CONTROL  0
+#define OFFSET_REVERB_CONTROL1   1
+#define OFFSET_REVERB_CONTROL2   2
+#define OFFSET_REVERB_CONTROL3   3
+#define OFFSET_REVERB_CONTROL4   4
+// clang-format on
 
 const size_t num_controls = 5;
 uint8_t controls[num_controls] = {
-	OFFSET_BITCRUSH_CONTROL,
-	OFFSET_REVERB_CONTROL1,
-	OFFSET_REVERB_CONTROL2,
-	OFFSET_REVERB_CONTROL3,
-	OFFSET_REVERB_CONTROL4,
+    OFFSET_BITCRUSH_CONTROL, // bitcrush
+    OFFSET_REVERB_CONTROL1,  // reverb
+    OFFSET_REVERB_CONTROL2,  //
+    OFFSET_REVERB_CONTROL3,  //
+    OFFSET_REVERB_CONTROL4,  //
 };
 
 using namespace daisysp;
@@ -39,93 +41,85 @@ dsy_gpio gate_output1, gate_output2;
 
 uint32_t gate_end = 0;
 
-static void AudioCallback(float *in, float *out, size_t size)
-{
-	float sig, dry_rate, send_rate, wet1, wet2;
+static void AudioCallback(float *in, float *out, size_t size) {
+  float sig, dry_rate, send_rate, wet1, wet2;
 
-	for (size_t i = 0; i < size; i += 2)
-	{
-		sig = in[i];
+  for (size_t i = 0; i < size; i += 2) {
+    sig = in[i];
 
-		if (clock.Process())
-		{
-			dsy_gpio_write(&gate_output1, gate1_state);
-			gate1_state = !gate1_state;
-		}
+    if (clock.Process()) {
+      dsy_gpio_write(&gate_output1, gate1_state);
+      gate1_state = !gate1_state;
+    }
 
-		dry_rate = verb_mix.Process();
-		send_rate = verb_send.Process();
-		bitcrush.SetFreq(bitcrush_rate.Process());
+    dry_rate = verb_mix.Process();
+    send_rate = verb_send.Process();
+    bitcrush.SetFreq(bitcrush_rate.Process());
 
-		sig = bitcrush.Process(sig);
+    sig = bitcrush.Process(sig);
 
-		verb.SetFeedback(verb_feedback.Process());
-		verb.SetLpFreq(verb_lp_freq.Process());
-		verb.Process(sig * send_rate, sig * send_rate, &wet1, &wet2);
+    verb.SetFeedback(verb_feedback.Process());
+    verb.SetLpFreq(verb_lp_freq.Process());
+    verb.Process(sig * send_rate, sig * send_rate, &wet1, &wet2);
 
-		out[i] = sig * dry_rate + wet1;
-		out[i + 1] = sig * dry_rate + wet2;
-	}
+    out[i] = sig * dry_rate + wet1;
+    out[i + 1] = sig * dry_rate + wet2;
+  }
 }
 
-void setup_gate_output(const int pin, dsy_gpio *gpio)
-{
-	gpio->pin = seed.GetPin(pin);
-	gpio->mode = DSY_GPIO_MODE_OUTPUT_PP;
-	gpio->pull = DSY_GPIO_NOPULL;
-	dsy_gpio_init(gpio);
-	dsy_gpio_write(gpio, false); // set low
+void setup_gate_output(const int pin, dsy_gpio *gpio) {
+  gpio->pin = seed.GetPin(pin);
+  gpio->mode = DSY_GPIO_MODE_OUTPUT_PP;
+  gpio->pull = DSY_GPIO_NOPULL;
+  dsy_gpio_init(gpio);
+  dsy_gpio_write(gpio, false); // set low
 }
 
-void setup_adcConfig(AdcChannelConfig *adcConfig, const int offset)
-{
-	adcConfig[offset].InitSingle(seed.GetPin(PIN_CONTROLS_BASE + offset));
+void setup_adcConfig(AdcChannelConfig *adcConfig, const int offset) {
+  adcConfig[offset].InitSingle(seed.GetPin(PIN_CONTROLS_BASE + offset));
 };
 
-int main(void)
-{
-	float sample_rate;
-	seed.Configure();
-	seed.Init();
-	sample_rate = seed.AudioSampleRate();
+int main(void) {
+  float sample_rate;
+  seed.Configure();
+  seed.Init();
+  sample_rate = seed.AudioSampleRate();
 
-	AdcChannelConfig adcConfig[num_controls];
-	for (uint8_t i = 0; i < num_controls; i++)
-	{
-		setup_adcConfig(adcConfig, controls[i]);
-	}
-	seed.adc.Init(adcConfig, num_controls);
+  AdcChannelConfig adcConfig[num_controls];
+  for (uint8_t i = 0; i < num_controls; i++) {
+    setup_adcConfig(adcConfig, controls[i]);
+  }
+  seed.adc.Init(adcConfig, num_controls);
 
-	// setup bitcrush
-	bitcrush.Init();
-	bitcrush_control.Init(seed.adc.GetPtr(OFFSET_BITCRUSH_CONTROL), sample_rate);
-	bitcrush_rate.Init(bitcrush_control, 0.001, 1.0, Parameter::EXPONENTIAL);
+  // setup bitcrush
+  bitcrush.Init();
+  bitcrush_control.Init(seed.adc.GetPtr(OFFSET_BITCRUSH_CONTROL), sample_rate);
+  bitcrush_rate.Init(bitcrush_control, 0.001, 1.0, Parameter::EXPONENTIAL);
 
-	//setup reverb
-	verb.Init(sample_rate);
+  // setup reverb
+  verb.Init(sample_rate);
 
-	verb_control1.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL1), sample_rate);
-	verb_feedback.Init(verb_control1, 0.f, 0.99f, Parameter::LINEAR);
+  verb_control1.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL1), sample_rate);
+  verb_feedback.Init(verb_control1, 0.f, 0.99f, Parameter::LINEAR);
 
-	verb_control2.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL2), sample_rate);
-	verb_lp_freq.Init(verb_control2, 0.f, 20000.0f, Parameter::EXPONENTIAL);
+  verb_control2.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL2), sample_rate);
+  verb_lp_freq.Init(verb_control2, 0.f, 20000.0f, Parameter::EXPONENTIAL);
 
-	verb_control3.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL3), sample_rate);
-	verb_mix.Init(verb_control3, 0.f, 1.0f, Parameter::LINEAR);
+  verb_control3.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL3), sample_rate);
+  verb_mix.Init(verb_control3, 0.f, 1.0f, Parameter::LINEAR);
 
-	verb_control4.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL4), sample_rate);
-	verb_send.Init(verb_control4, 0.f, 1.0f, Parameter::LINEAR);
+  verb_control4.Init(seed.adc.GetPtr(OFFSET_REVERB_CONTROL4), sample_rate);
+  verb_send.Init(verb_control4, 0.f, 1.0f, Parameter::LINEAR);
 
-	clock.Init(1.0f, sample_rate);
+  clock.Init(1.0f, sample_rate);
 
-	setup_gate_output(PIN_GATE_OUT1, &gate_output1);
-	setup_gate_output(PIN_GATE_OUT2, &gate_output2);
+  setup_gate_output(PIN_GATE_OUT1, &gate_output1);
+  setup_gate_output(PIN_GATE_OUT2, &gate_output2);
 
-	// start callback
-	seed.adc.Start();
-	seed.StartAudio(AudioCallback);
+  // start callback
+  seed.adc.Start();
+  seed.StartAudio(AudioCallback);
 
-	while (1)
-	{
-	}
+  while (1) {
+  }
 }
