b4c336e0f65f8284dc1293c3dcb2a58798f68c03
Hiroaki Yutani <yutani.ini@gmail.com>
2021-05-05 17:00:13 +0900

    Implement substep

--- daisy/pattern.cpp
+++ daisy/pattern.cpp
@@ -7,25 +7,31 @@
 #define MASK_GATE2_ON    (uint8_t)(1 << 4) // 0001 0000
 #define MASK_GATE2_DELAY (uint8_t)(1 << 5) // 0010 0000
 
-#define LENGTH_ONESHOT   0.124f
+#define LENGTH_ONESHOT 0.124f
+#define LENGTH_LONG    1.0f
 // clang-format on
 
 using namespace gasstove;
 
 struct SubStep {
-  float length; // gate length
-  float delay;  // delay before the gate starts
-  float sleep;  // delay before the next substep
+  float start;  // when the gate starts **in phase**
+  float length; // gate length **in seconds**
 };
 
-// clang-format off
-const SubStep substep_pattern1[] = {
-  SubStep{1.0f, 0.0f, 0.0f},
-  SubStep{1.0f, 0.0f, 1.0f},
-  SubStep{0.3f, 0.0f, 0.1f},
-  SubStep{1.0f, 0.0f, 1.0f},
+struct SubStepPattern {
+  uint8_t n;
+  SubStep substeps[4];
 };
-// clang-format on
+
+SubStepPattern substep_pattern1 = {
+  n : 4,
+  substeps : {
+      SubStep{0.0f, LENGTH_ONESHOT},
+      SubStep{0.0f, LENGTH_ONESHOT},
+      SubStep{0.5f, LENGTH_ONESHOT},
+      SubStep{0.75f, LENGTH_ONESHOT},
+  }
+};
 
 void Pattern::Init(float sample_rate, Clock *clock, Gate *gate1, Gate *gate2) {
   sample_rate_ = sample_rate;
@@ -43,14 +49,16 @@ void Pattern::Init(float sample_rate, Clock *clock, Gate *gate1, Gate *gate2) {
 
 void Pattern::Process() {
   if (clock_->Process()) {
-    cur_substep_ = 0;
-
-    if (true) {
-      gates_[cur_gate_]->Trigger(LENGTH_ONESHOT, 0.0f);
-      cur_gate_ = (cur_gate_ + 1) % num_of_gates;
-    }
-
     cur_step_ = (cur_step_ + 1) % max_step_;
+    cur_substep_ = 0;
+  }
+
+  if (substep_pattern1.substeps[cur_substep_].start <= clock_->GetPhase() &&
+      cur_substep_ < substep_pattern1.n) {
+    gates_[cur_gate_]->Trigger(substep_pattern1.substeps[cur_substep_].length,
+                               0.0f);
+    cur_gate_ = (cur_gate_ + 1) % num_of_gates;
+    cur_substep_ = (cur_substep_ + 1);
   }
 
   for (uint8_t i = 0; i < num_of_gates; i++) {
